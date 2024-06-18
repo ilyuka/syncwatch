c8746ff9c0a82a2a823ce6ac485c2f2ab4cb9af6
ilya <theilyashulepov@gmail.com>
2024-06-18 23:51:48 +0300

    enable cors middleware

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,21 @@
-import express, { Express, Request, Response } from "express";
+import http from "node:http";
 import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import socketIO from "socket.io";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const httpServer = http.createServer(app);
+const io = new socketIO.Server(httpServer);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("server response");
 });
 
+app.use(cors());
 app.listen(port, () => {
   console.log(`[server]: listening on port ${port}`);
 });
